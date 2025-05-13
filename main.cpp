d4e1f92386264d961556cb38b100fb0fe16c5f9a
Guest-11111111 <nanda.swagatika@gmail.com>
2025-05-12 20:34:47 -0500

    Update main.cpp
    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,17 +7,18 @@
  * Fill this section in with some information about your app.
  * All fields are optional - so if you don't need one, take it out.
  */
-APP_NAME("My app name")
-APP_DESCRIPTION("A short description of my app")
-APP_AUTHOR("My name")
-APP_VERSION("1.0.2")
+APP_NAME("Physics Formulae")
+APP_DESCRIPTION(" Physics solver (BASIC)")
+APP_AUTHOR("Sai Nanda")
+APP_VERSION("1.0.0")
 
 extern "C"
 void main() {
 	calcInit(); //backup screen and init some variables
 
-	cout << "Hello, World"
-	int cin << x 
+	cout << "";
+	int cin << x ;
+	
 	
 
 	//Example for setPixel(x,y,color)
